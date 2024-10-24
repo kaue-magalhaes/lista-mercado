7b6cf80f3a58e6e715db67bb01eaa9e1c233aa2d
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-10-24 19:40:29 -0300

    init the mobile project

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { style } from "./style";
+
+export default function Index() {
+    return (
+        <View style={style.container}>
+            <Text>Hello World!</Text>
+        </View>
+    )
+}
