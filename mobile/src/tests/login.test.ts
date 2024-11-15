831dfdc466ab93b56cdbbf3d47805cd2027dc20f
Kauê de Magalhães <kauedemagalhaes27@gmail.com>
2024-11-15 17:06:21 -0300

    melhorando a organização do código

--- mobile/src/tests/login.test.ts
+++ mobile/src/tests/login.test.ts
@@ -1,26 +1,82 @@
 import { renderRouter, screen, userEvent } from 'expo-router/testing-library';
-import { login } from '@/actions/auth/login';
-import Login from '@/app/login';
-import Home from '@/app/(app)';
+import { UserEventInstance } from '@testing-library/react-native/build/user-event/setup';
+
 import { Platform } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
+
+import Login from '@/app/login';
+import Home from '@/app/(app)';
+
+import { login } from '@/actions/auth/login';
 import { postApi } from '@/actions/api/post';
-import { setToken } from '@/actions/auth/token';
-import { fetchUser } from '@/actions/auth/user';
+import { setToken } from '@/actions/auth/token/setToken';
+import { fetchUser } from '@/actions/auth/user/fetch';
 
 describe('Login Flow Test Suite', () => {
     const mockCredentials = {
         email: 'test@example.com',
         password: 'password',
-        device_name: `${Platform.OS} ${Platform.Version}`
-    }
+        device_name: `${Platform.OS} ${Platform.Version}`,
+    };
 
     const mockToken = { token: 'mock-token' };
 
+    const mockLogin = (isImplementationTest: boolean) => {
+        if (isImplementationTest) {
+            return jest.spyOn(require('@/actions/auth/login'), login.name);
+        }
+        return jest.spyOn(require('@/actions/auth/login'), login.name).mockResolvedValue(null);
+    }
+
+    const mockPostApi = (isImplementationTest: boolean) => {
+        if (isImplementationTest) {
+            return jest.spyOn(require('@/actions/api/post'), postApi.name);
+        }
+        return jest.spyOn(require('@/actions/api/post'), postApi.name).mockResolvedValue(mockToken);
+    }
+
+    const mockSetToken = (isImplementationTest: boolean) => {
+        if (isImplementationTest) {
+            return jest.spyOn(require('@/actions/auth/token/setToken'), setToken.name);
+        }
+        return jest.spyOn(require('@/actions/auth/token/setToken'), setToken.name).mockResolvedValue(null);
+    }
+
+    const mockFetchUser = (isImplementationTest: boolean) => {
+        if (isImplementationTest) {
+            return jest.spyOn(require('@/actions/auth/user/fetch'), fetchUser.name);
+        }
+        return jest.spyOn(require('@/actions/auth/user/fetch'), fetchUser.name).mockResolvedValue(null);
+    }
+
+    const mockSecureStore = () => {
+        jest.spyOn(SecureStore, 'setItemAsync').mockResolvedValue(Promise.resolve());
+    }
+
+    const mockLocalStorage = () => {
+        global.localStorage = {
+            getItem: jest.fn(),
+            setItem: jest.fn(),
+            removeItem: jest.fn(),
+            clear: jest.fn(),
+            key: jest.fn(),
+            length: 0,
+        };
+    }
+
+    const fillAndSubmitLoginForm = async (user: UserEventInstance) => {
+        await user.type(screen.getByPlaceholderText('Enter your email'), mockCredentials.email);
+        await user.type(screen.getByPlaceholderText('Enter your password'), mockCredentials.password);
+        await user.press(screen.getByText('Login'));
+    };
+
     beforeEach(() => {
+        // Limpa todos os mocks
         jest.clearAllMocks();
+
+        // Mocka as rotas
         renderRouter(
-            { login: Login, '/(app)/': Home }, 
+            { login: Login, '/(app)/': Home },
             { initialUrl: '/login' }
         );
     });
@@ -34,103 +90,41 @@ describe('Login Flow Test Suite', () => {
             expect(screen.getByPlaceholderText('Enter your email')).toBeTruthy();
             expect(screen.getByPlaceholderText('Enter your password')).toBeTruthy();
             expect(screen.getByText('Login')).toBeTruthy();
-    
+
             expect(screen).toHavePathname('/login');
         });
     });
 
     describe('Login Action', () => {
-        it('should call the login action with the correct data when the form is submitted', async() => {
-            // arrange
+        it('should call the login action with the correct data when the form is submitted', async () => {
             const user = userEvent.setup();
-            jest.spyOn(
-                    require('@/actions/auth/login'), 
-                    login.name
-                )
-                .mockImplementation(() => Promise.resolve());
-            // act
-            await user.type(screen.getByPlaceholderText('Enter your email'), mockCredentials.email);
-            await user.type(screen.getByPlaceholderText('Enter your password'), mockCredentials.password);
-            await user.press(screen.getByText('Login'));
-            // assert
+            const login = mockLogin(false);
+
+            await fillAndSubmitLoginForm(user);
+
             expect(login).toHaveBeenCalledTimes(1);
             expect(login).toHaveBeenCalledWith(mockCredentials);
         });
-    
-        test('when calling the login action, a POST request must be made to the backend passing the data correctly', async() => {
-            // arrange
-            // mock postApi
-            jest.spyOn(
-                    require('@/actions/api/post'),
-                    postApi.name
-                )
-                .mockResolvedValue(mockToken);
 
-            // mock setToken
-            jest.spyOn(
-                    require('@/actions/auth/token/setToken'),
-                    setToken.name,
-                )
-                .mockResolvedValue(null);
-                
-            // mock fetchUser
-            jest.spyOn(
-                    require('@/actions/auth/user/fetch'),
-                    fetchUser.name
-                )
-                .mockResolvedValue(null);
-    
-            // act
+        it('should send a POST request with the correct data', async () => {
+            mockPostApi(false);
+            mockSetToken(false);
+            mockFetchUser(false);
+            
             await login(mockCredentials);
-    
-            // assert
-            expect(postApi).toHaveBeenCalledWith(
-                '/api/login',
-                { body: JSON.stringify(mockCredentials) }
-            );
+
+            expect(postApi).toHaveBeenCalledWith('/api/login', { body: JSON.stringify(mockCredentials) });
         });
-    
-        it('should receive and store the token returned by the backend', async() => {
-            // arrange
-            // mock postApi
-            jest.spyOn(
-                    require('@/actions/api/post'),
-                    postApi.name
-                )
-                .mockResolvedValue(mockToken);
-    
-            // mock setToken
-            jest.spyOn(
-                    require('@/actions/auth/token/setToken'),
-                    setToken.name,
-                );
 
-            // mock fetchUser
-            jest.spyOn(
-                    require('@/actions/auth/user/fetch'),
-                    fetchUser.name
-                )
-                .mockResolvedValue(null);
+        it('should store the token returned by the backend', async () => {
+            mockPostApi(false);
+            mockSetToken(true);
+            mockFetchUser(false);
+            mockSecureStore();
+            mockLocalStorage();
 
-            jest.spyOn(
-                    SecureStore,
-                    'setItemAsync'
-                )
-                .mockResolvedValue(Promise.resolve());
-        
-            global.localStorage = {
-                getItem: jest.fn(),
-                setItem: jest.fn(),
-                removeItem: jest.fn(),
-                clear: jest.fn(),
-                key: jest.fn(),
-                length: 0
-            };
-    
-            // act
             await login(mockCredentials);
-    
-            // assert
+
             expect(setToken).toHaveBeenCalledWith(mockToken.token);
             if (Platform.OS === 'web') {
                 expect(localStorage.setItem).toHaveBeenCalledWith('token', mockToken.token);
@@ -138,52 +132,22 @@ describe('Login Flow Test Suite', () => {
                 expect(SecureStore.setItemAsync).toHaveBeenCalledWith('token', mockToken.token);
             }
         });
-    
-        it('should send a request to receive and store the logged in user data', async() => {
-            // arrange
-            // mock postApi
-            jest.spyOn(
-                    require('@/actions/api/post'),
-                    postApi.name
-                )
-                .mockResolvedValue(mockToken);
-    
-            // mock setToken
-            jest.spyOn(
-                    require('@/actions/auth/token/setToken'),
-                    setToken.name,
-                )
-                .mockResolvedValue(null);
 
-            // mock fetchUser
-            jest.spyOn(
-                    require('@/actions/auth/user/fetch'),
-                    fetchUser.name
-                )
-                .mockResolvedValue(null);
-    
-            // act
+        it('should fetch and store the logged-in user data', async () => {
+            mockPostApi(false);
+            mockSetToken(false);
+            mockFetchUser(false);
             await login(mockCredentials);
-    
-            // assert
+
             expect(fetchUser).toHaveBeenCalledTimes(1);
         });
-    
-        it('should redirect the user to the home page', async() => {
-            // arrange
+
+        it('should redirect the user to the home page after login', async () => {
             const user = userEvent.setup();
-            jest.spyOn(
-                require('@/actions/auth/login'), 
-                login.name
-            )
-            .mockImplementation(() => Promise.resolve());
-           
-            // act
-            await user.type(screen.getByPlaceholderText('Enter your email'), mockCredentials.email);
-            await user.type(screen.getByPlaceholderText('Enter your password'), mockCredentials.password);
-            await user.press(screen.getByText('Login'));
-    
-            // assert
+            mockLogin(false);
+
+            await fillAndSubmitLoginForm(user);
+
             expect(screen).toHavePathname('/(app)');
         });
     });
